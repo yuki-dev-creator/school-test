1780c3aedebfca68c2df2414cddafa36ae440b82
yuki-dev-creator <kristina_kd_12@bk.ru>
2026-02-27 21:41:22 +0300

    добавление страницы - категории, изменение карточек

---
+++ scripts/task.js
@@ -0,0 +1,150 @@
+const app = document.getElementById("app");
+const nextBtn = document.getElementById("next");
+const progressTxt = document.getElementById("progress");
+const againBtn = document.getElementById("again");
+const backgroundAudio = new Audio("../audio/backgroundAudio.mp3");
+const musicBtn = document.getElementById("music");
+const failAudio = new Audio("../audio/failAudio.mp3");
+const winAudio = new Audio("../audio/winAudio.mp3");
+const timeTxt = document.getElementById("time");
+let time = 0;
+let isPlaying = true;
+
+for (let i = 0; i < questions.length; i++) {
+  const element = questions[i];
+  console.log(element.time);
+  time = time + element.time;
+}
+
+// first time show
+//time (seconds) -> 2:00
+
+backgroundAudio.volume = 0.3;
+backgroundAudio.play();
+backgroundAudio.onended = function () {
+  backgroundAudio.currentTime = 0;
+  backgroundAudio.play();
+};
+
+function finishTest() {
+  clearInterval(timerInterval);
+  nextBtn.style.display = "none";
+  const isWin = score > questions.length / 2;
+  let text = isWin ? "Good job!" : "Try again :(";
+  let iconStatus = isWin ? "success" : "error";
+  backgroundAudio.pause();
+  if (isWin) winAudio.play();
+  else failAudio.play();
+
+  Swal.fire({
+    title: text,
+    text: `your score is ${score} / ${questions.length}`,
+    icon: iconStatus,
+  });
+}
+
+function timeOn() {
+  let seconds = time % 60;
+  let minutes = Math.floor(time / 60);
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  let localTime = minutes + ":" + seconds;
+  timeTxt.textContent = localTime;
+}
+const timerInterval = setInterval(function () {
+  time--;
+  timeOn();
+
+  if (time <= 0) {
+    finishTest();
+  }
+}, 1000);
+
+let currentIndex = 0;
+let score = 0;
+
+function renderQuestion() {
+  const question = questions[currentIndex];
+  console.log(question);
+
+  app.innerHTML = "";
+
+  progressTxt.textContent = `Вопрос № ${currentIndex + 1}`;
+  const title = document.createElement("h3");
+  title.textContent = question.question;
+  title.classList.add("question-txt");
+  app.appendChild(title);
+
+  const label = document.createElement("label");
+  label.textContent = "Enter answer";
+  label.classList.add("question-label");
+  const input = document.createElement("input");
+  input.type = "text";
+  input.name = "answer";
+
+  label.appendChild(input);
+  app.appendChild(label);
+}
+
+musicBtn.onclick = function () {
+  if (isPlaying) {
+    backgroundAudio.pause();
+    musicBtn.textContent = "play";
+  } else {
+    backgroundAudio.play();
+    musicBtn.textContent = "stop playing music";
+  }
+  isPlaying = !isPlaying;
+};
+
+nextBtn.onclick = function () {
+  const selected = document.querySelectorAll('input[name="answer"]:checked');
+  console.log(selected);
+  if (selected.length === 0) {
+    Swal.fire({
+      title: "Error!",
+      text: "Do you want to continue",
+      icon: "error",
+      confirmButtonText: "okay",
+    });
+    return;
+  }
+
+  const usersValues = Array.from(selected).map((item) => +item.value);
+  const correctValues = questions[currentIndex].correct;
+  const isCorrect =
+    usersValues.length === correctValues.length &&
+    usersValues.every((val) => correctValues.includes(val));
+
+  if (isCorrect === true) {
+    score++;
+  }
+
+  currentIndex++;
+
+  if (currentIndex === questions.length - 1) {
+    nextBtn.textContent = "end test";
+  }
+
+  if (currentIndex < questions.length) {
+    renderQuestion();
+  } else {
+    finishTest();
+  }
+};
+
+againBtn.onclick = function () {
+  currentIndex = 0;
+  score = 0;
+  nextBtn.style.display = "inline-block";
+  nextBtn.textContent = "next";
+
+  renderQuestion();
+};
+
+renderQuestion();
+timeOn();
